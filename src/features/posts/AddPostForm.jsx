846eb84d2ce048bd70d205bd03d7b9caece1422d
lvxianru <lvxianru@rongma.com>
2022-07-05 17:39:25 +0800

    init

---
+++ src/features/posts/AddPostForm.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { postAdded } from './postsSlice'
+
+export const AddPostForm = () => {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+  const [userId, setUserId] = useState('')
+
+  const dispatch = useDispatch()
+  const users = useSelector((state) => state.users)
+
+  const onTitleChanged = (e) => setTitle(e.target.value)
+  const onContentChanged = (e) => setContent(e.target.value)
+  const onAuthorChanged = (e) => setUserId(e.target.value)
+
+  const onSavePostClicked = () => {
+    if (title && content) {
+      dispatch(postAdded(title, content, userId))
+      setTitle('')
+      setContent('')
+    }
+  }
+
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+
+  const usersOptions = users.map((user) => (
+    <option value={user.id} key={user.id}>
+      {user.name}
+    </option>
+  ))
+
+  return (
+    <section>
+      <h2>添加新帖子</h2>
+      <form>
+        <label htmlFor="postTitle">帖子标题：</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleChanged}
+        />
+        <label htmlFor="postAuthor">Author：</label>
+        <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
+          <option value=""></option>
+          {usersOptions}
+        </select>
+        <label htmlFor="postContent">内容：</label>
+        <textarea
+          name="postContent"
+          id="postContent"
+          value={content}
+          onChange={onContentChanged}
+        />
+        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
+          保存帖子
+        </button>
+      </form>
+    </section>
+  )
+}
